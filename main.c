9670c31c05abdb5cdc8796a859f97d069e00f8c8
Alessandro Panait <14529922@itisrossi.vi.it>
2026-03-24 10:09:41 +0100

    struttura base di main.c

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+// Costante magica: entrambi la cambieranno!
+#define VALORE 0
 
-int main(void) {
-    printf("Hello, World!\n");
+int main() {
+    printf("Il valore e': %d\n", VALORE);
     return 0;
 }
